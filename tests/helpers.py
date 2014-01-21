70a27a90c84707872767e8f971b90e23d747c07c
Kevin Murray <spam@kdmurray.id.au>
2014-01-21 17:45:22 +1100

    add tests module and test helpers

---
+++ tests/helpers.py
@@ -0,0 +1,10 @@
+
+# SKIP messages
+SKIP_NEED_INTERNET = "Test requires internet access"
+SKIP_NEED_LINUX = "Test must be run on a linux-based system"
+SKIP_NEED_MACOSX = "Test must be run on a Mac OSX system"
+SKIP_NEED_WINDOWS = "Test must be run on a Windows system"
+
+# Assert Constants
+LOCALHOST_IP = "127.0.0.1"
+
